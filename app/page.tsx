23a112d00105a39b4d2d1cd10e55298469a5624f
bayl0n <nathanbaylon@gmail.com>
2023-08-19 03:03:29 +1000

    feat: :sparkles: Create budget calculator feature

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ import {
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { NavigationBar } from "@/components/navigation-bar"
 import { DollarSign, Users } from "lucide-react"
-import { SalaryCalculatorCard } from "@/components/salary-calculator"
+import { BudgetDashboard} from "@/components/budget-calculator"
 
 export const metadata: Metadata = {
   title: "Rakan",
@@ -123,15 +123,20 @@ export default function HomePage() {
     <div className="flex flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <NavigationBar />
-        <Tabs defaultValue="apartment" className="space-y-4">
+        <Tabs defaultValue="personal" className="space-y-4">
           <TabsList>
-            <TabsTrigger value="apartment">
-              Apartment
-            </TabsTrigger>
             <TabsTrigger value="personal">
               Personal
             </TabsTrigger>
+            <TabsTrigger value="apartment">
+              Apartment
+            </TabsTrigger>
           </TabsList>
+          <TabsContent value="personal" className="space-y-4">
+            <div className="flex flex-col-reverse gap-4 sm:grid md:grid-cols-2 lg:grid-cols-4">
+              <BudgetDashboard/>
+            </div>
+          </TabsContent>
           <TabsContent value="apartment" className="space-y-4">
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
               <Card>
@@ -206,67 +211,6 @@ export default function HomePage() {
                 <CardContent>
                 </CardContent>
               </Card>
-            </div>
-          </TabsContent>
-          <TabsContent value="personal" className="space-y-4">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Rent this month
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">$852.41</div>
-                  <p className="text-xs text-muted-foreground">
-                    +20.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Expenses
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">
-                    ${invoices.map(invoice => invoice.totalAmount).reduce((a, b) => a + b)}.00
-                  </div>
-                  <p className="text-xs text-muted-foreground">
-                    +180.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-              <SalaryCalculatorCard/>
             </div>
           </TabsContent>
         </Tabs>
---
+++ components/budget-calculator.tsx
@@ -0,0 +1,126 @@
+"use client"
+
+import { Calculator } from "lucide-react";
+import { Card, CardHeader, CardTitle, CardContent } from "./ui/card";
+import { Input } from "./ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+const formSchema = z.object({
+    grossIncome: z.coerce.number().min(0, {
+        message: "Gross income must be greater than 0.",
+    }),
+});
+
+export function BudgetDashboard({...props}) {
+    const [grossIncome, useGrossIncome] = useState(50000);
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+        useGrossIncome(values.grossIncome);
+    }
+
+    return (
+        <>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                        Gross Income
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">
+                        ${grossIncome.toLocaleString(undefined, {minimumFractionDigits: 2})}
+                    </div>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                        Expenses
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">
+                        ${(grossIncome * 0.5).toLocaleString(undefined, {minimumFractionDigits: 2})}
+                    </div>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                        Luxury
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">
+                        ${(grossIncome * 0.3).toLocaleString(undefined, {minimumFractionDigits: 2})}
+                    </div>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                        Savings
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">
+                        ${(grossIncome * 0.2).toLocaleString(undefined, {minimumFractionDigits: 2})}
+                    </div>
+                </CardContent>
+            </Card>
+            <BudgetCalculatorForm grossIncome={grossIncome} onSubmit={onSubmit} />
+        </>
+    )
+}
+
+export function BudgetCalculatorForm({ grossIncome, onSubmit }: { grossIncome: number, onSubmit: any }) {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            grossIncome: grossIncome,
+        },
+    });
+
+    return (
+        <Card className="col-span-3">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-6">
+                <CardTitle className="text-xl font-medium">
+                    Salary Calculator
+                </CardTitle>
+                <Calculator
+                    className="h-4 w-4 text-muted-foreground"
+                />
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="grossIncome"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Gross Income</FormLabel>
+                                    <FormControl>
+                                        <Input type="number" placeholder="Gross Income" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is your gross income.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    )
+}
