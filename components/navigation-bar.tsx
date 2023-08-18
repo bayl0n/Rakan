959f0b22cff42176fbe9a71ee5f96de68322c1e9
bayl0n <nathanbaylon@gmail.com>
2023-08-19 03:13:39 +1000

    style: :wheelchair: Change logo to h1

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -18,9 +18,9 @@ export function NavigationBar() {
     return (
         <div className="flex items-center justify-between space-y-2">
             <div className="flex">
-                <h2 className="tracking-tight transition font-display text-3xl font-bold sm:text-[3rem] bg-gradient-to-r from-20% bg-clip-text text-transparent from-emerald-500 to-yellow-500 dark:from-emerald-400 dark:to-yellow-300">
+                <h1 className="tracking-tight transition font-display text-3xl font-bold sm:text-[3rem] bg-gradient-to-r from-20% bg-clip-text text-transparent from-emerald-500 to-yellow-500 dark:from-emerald-400 dark:to-yellow-300">
                     Rakan
-                </h2>
+                </h1>
                 <BirdIcon className="text-yellow-500 dark:text-yellow-300 text-6xl text-[3rem]" />
                 <NavigationMenu className="mx-4 hidden sm:block">
                     <NavigationMenuList>
